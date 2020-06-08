33672a6d6fcb9fc95f65353666c4a9de11a4ff99
LE DUC LINH <linhducle123@gmail.com>
2020-06-08 10:41:29 +0700

    fix cors6

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var timeout = require('connect-timeout')
 
 require('dotenv').config();
 const port = process.env.PORT || 8080;
-app.use(timeout('15s'))
+// app.use(timeout('15s'))
 // app.use(cors());
 app.use(cookieParser());
 app.use(function(req, res, next) {
